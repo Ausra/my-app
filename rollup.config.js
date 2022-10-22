4613303aa7d5d76777a1e9f74ac7f22774a86f27
Ausra <ausrab@wix.com>
2022-10-22 15:59:05 +0300

    added storybook

--- rollup.config.js
+++ rollup.config.js
@@ -29,7 +29,7 @@ export default [
         peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript( {tsconfig: "./tsconfig.json" }),
       postcss(), 
       terser(),
     ],
