b449045e15d76b574b7f9181240a5b589b172009
Ausra <ausrab@wix.com>
2022-10-22 14:02:57 +0300

    added app optimization plugnis

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
@@ -24,10 +26,12 @@ export default [
       },
     ],
     plugins: [
+        peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(), 
+      terser(),
     ],
   },
   {
